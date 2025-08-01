7d7e896725ef774a81dbf827f86828a7e9a7ddf2
Laxmikant3132 <laxmikanttalli303@gmail.com>
2025-08-01 13:16:15 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: ' https://backend-project-un5v.onrender.com',
         changeOrigin: true,
       }
     }
